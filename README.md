47f59f43af357c2b84608c364f7cc161c91f5ba7
Brent Pease <brenthpease2@gmail.com>
2015-12-27 21:16:31 -0800

    Prep for first release

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Embedded Library
 ========================================
-A framework for developing moderate to very complex ARM based Arduino sketches. At the core of the framework is
-the concept of Modules. All functionality is provided through modules allowing sketches to only include what it
+A framework for developing moderate to very complex ARM based Arduino sketches in C++. At the core of the framework 
+is the concept of Modules. All functionality is provided through modules allowing sketches to only include what it
 needs. Each module can have the follwing:
 
  - Its own EEPROM space that is managed by the framework
@@ -10,8 +10,8 @@ needs. Each module can have the follwing:
  
 Another core concept is being event based. The sketch registers event handlers to implement the unique
 functionality of the sketch. An event handler is just a method on a C++ object. For example when a digital
-IO pin becomes active a specific event handler is called. The framework handles the details of configuring
-the IO pin, managing the state, and handling debouncing.
+IO pin becomes active the user's registered event handler is called for that pin. The framework handles the details
+of configuring the IO pin, managing the state, and handling debouncing.
  
 In addition to the module system this library provides the following functionality:
  - Handling of commands coming over the serial port in a similar fashion to linux command line tools with
@@ -24,8 +24,8 @@ In addition to the module system this library provides the following functionali
  - A touch module for the Teensyduino analog touch input pins.
  - A simple config value module for setting and getting persistent global config data.
  - A module for computing sunrise and sunset times as well as calling event handlers for sunset and sunrise events
- - A debugging facility for asserts and reporting errors to the user
- - A CANBus event handling modules (still in active development and not fully tested yet)
+ - A debugging facility for asserts and reporting errors to the user over the serial port
+ - A CANBus event handling module (still in active development and not fully tested yet)
  - A module for the Sparkfun TSL2561 luminosity sensor
 
 Repository Contents
@@ -36,7 +36,8 @@ A zip file of the library that can easily be installed as described below
 Installation Instructions
 -------------------------
 
-Extract the contents of the zip file and place into your Documents/Arduino/libraries/ folder
+Extract the contents of the zip file and place into your Documents/Arduino/libraries/ folder. Rename the enclosed
+folder from "EmbeddedLibrary-master" to just "EmbeddedLibrary".
 
 Documentation
 --------------
@@ -44,6 +45,17 @@ Documentation
 An example sketch is provided and the headers files have lots of comments descriping the API
 calls.
 
+A Few Notes
+-----------
+My development environment is as follows:
+ - Windows 7 64-bit
+ - Visual Studio 2015 Community Edition (https://www.visualstudio.com/en-us/products/visual-studio-community-vs.aspx), its free.
+ - Visual Micro, also free (http://www.visualmicro.com/)
+ - Visual Assist X (http://www.wholetomato.com/) which is not free but well worth the $99.
+ - Teensyduino 3.x
+ 
+ I mention this because this library is very new and has not been tested outside of this setup. 
+ 
 License Information
 -------------------
 
---
+++ examples/BasicExample/BasicExample.ino
@@ -0,0 +1,131 @@
+/*
+	Author: Brent Pease
+
+	The MIT License (MIT)
+
+	Copyright (c) 2015-FOREVER Brent Pease
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all
+	copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+	SOFTWARE.
+*/
+
+/*
+	ABOUT
+
+	This is the main ino for the BasicExample example sketch in the EmbeddedLibrary library
+*/
+
+#include <EEPROM.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <FlexCAN.h>
+#include <SparkFunTSL2561.h>
+
+#include <ELModule.h>
+#include <ELDigitalIO.h>
+#include <ELSerial.h>
+#include <ELRealTime.h>
+#include <ELAssert.h>
+
+class CExampleModule : public CModule, public IDigitalIOEventHandler, public ISerialCmdHandler, public IRealTimeHandler
+{
+	CExampleModule(
+		)
+		:
+		CModule(
+			"expl",		// This is the 4 character name of the module, every module has a unique 4 character code.
+			0,			// This is the amount of EEPROM space this module needs
+			0,			// This is the version number of the EEPROM data format. If this changes the system will re-initialize the EEPROM data for you
+			30000)		// This is the period for calling the Update function
+	{
+	}
+
+	virtual void
+	Setup(
+		void)
+	{
+		// this will be called after the other system level modules have been initialized
+		Serial.printf("In Setup\n");
+
+		// Register a serial port command.
+		gSerialCmd->RegisterCommand("hello", this, static_cast<TSerialCmdMethod>(&CExampleModule::SerialCmdHello));
+
+		// Register a digital io event handler on pin 1
+		gDigitalIO->RegisterEventHandler(1, false, this, static_cast<TDigitalIOEventMethod>(&CExampleModule::PinActivated), NULL);
+
+		// Register an event to fire every 5 seconds
+		gRealTime->RegisterEvent("MyEvent", 5 * 1000000, false, this, static_cast<TRealTimeEventMethod>(&CExampleModule::MyPeriodicEvent), NULL);
+
+		updateCount = 0;
+	}
+
+	virtual void
+	Update(
+		uint32_t	inDeltaTimeUS)
+	{
+		// this is called apprx every 30ms as specified in the module constructor above
+		++updateCount;
+	}
+
+	bool
+	SerialCmdHello(
+		int		inArgC,
+		char*	inArgv[])
+	{
+		Serial.printf("Hello World\n");
+
+		return true;	// Returning true means command was processed successfully, returning false will print an error to serial out
+	}
+
+	void
+	PinActivated(
+		uint8_t		inPin,
+		EPinEvent	inEvent,
+		void*		inReference)
+	{
+		Serial.printf("pin=%d event=%d\n", inPin, inEvent);
+	}
+
+	void
+	MyPeriodicEvent(
+		char const*	inName,
+		void*		inRef)
+	{
+		DebugMsg(eDbgLevel_Basic, "event %s has fired", inName);
+		DebugMsg(eDbgLevel_Basic, "updateCount = %d", updateCount);
+	}
+
+	int	updateCount;
+
+	static CExampleModule	module;
+};
+CExampleModule	CExampleModule::module;
+
+void
+setup(
+	void)
+{
+	CModule::SetupAll(true);	// Passing in true blinks the led
+}
+
+void
+loop(
+	void)
+{
+	CModule::LoopAll();
+}
